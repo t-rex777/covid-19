4d24c4417dcac219327a3768c285b41a3b141c87
Manish Kumar Sahu <56619260+t-rex777@users.noreply.github.com>
2020-04-20 22:58:18 +0530

    did some final padding and margin changes and changed the real favicon

--- src/components/CountryPicker/CountryPicker.jsx
+++ src/components/CountryPicker/CountryPicker.jsx
@@ -17,7 +17,8 @@ function CountryPicker({handleCountryChange}){
     return(<FormControl className={styles.FormControl}>
         <NativeSelect defaultValue="" onChange={(e) => handleCountryChange(e.target.value)}>
         <option value = ''>Global</option>
-        {fetchedCountries.map((country, i ) => <option key={i} value={country}>{country}</option>)}
+        {fetchedCountries.map((country, i ) => <option key={i} value={country}>{country}
+        </option>)}
         </NativeSelect>
         </FormControl>)
 }
